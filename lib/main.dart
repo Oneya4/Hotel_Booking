d99d5eadcd5b4a1b53b389a5953ec2dd41fef44c
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-21 16:15:09 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hotel_booking/home_page.dart';
+
+import '/screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const HomeScreen(),
     );
   }
 }
