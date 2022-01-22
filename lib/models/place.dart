ad7d36f071b3bd8dce19cef61223eaeb31c673d5
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-22 22:49:00 +0300

    Create place.dart

---
+++ lib/models/place.dart
@@ -0,0 +1,17 @@
+class Place {
+  String id;
+  String location;
+  String name;
+  String imageUrl;
+  double cost;
+  double reviews;
+
+  Place({
+    required this.id,
+    required this.location,
+    required this.name,
+    required this.imageUrl,
+    required this.cost,
+    required this.reviews,
+  });
+}
