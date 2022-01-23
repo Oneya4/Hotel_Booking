58263174ec5f3180fc0b58c040d01a52464786d0
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-23 22:32:16 +0300

    Create nav_bar.dart

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue[400],
+        unselectedItemColor: Colors.blueGrey[200],
+        iconSize: 30,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Places',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_alert_rounded),
+            label: 'Offers',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_outlined),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
